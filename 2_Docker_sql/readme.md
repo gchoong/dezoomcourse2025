60c08949a608e40ca3bf9927df52d376360cf913
gchoong <108086222+gchoong@users.noreply.github.com>
2025-01-16 15:16:53 -0600

    refreshing docker and adding jupyter notebook

--- 2_Docker_sql/readme.md
+++ 2_Docker_sql/readme.md
@@ -1,3 +1,20 @@
 #Question 1 
 
 -pip --version
+
+# Commands
+## Creating the postgres database in docker
+docker run -it \
+  -e POSTGRES_DB=ny_taxi \
+  -e POSTGRES_USER=postgres \
+  -e POSTGRES_PASSWORD=postgres \
+  -v ./ny_taxi_postgres_data:/var/lib/postgresql/data \
+  -p 5432:5432 \
+  --name pgdatabase \
+  postgres:17-alpine
+
+## accessing the postgres with pgcli
+pgcli \
+	-h  localhost -p 5432 \
+	-u postgres \
+	-d ny_taxi
--- 2_Docker_sql/upload_data.py
+++ 2_Docker_sql/upload_data.py
@@ -1,2 +1,22 @@
 import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/ny_taxi')
+
+engine.connect
+
+df = pd.read_csv('green_tripdata_2019-10.csv', nrows=100)
+
+# print(df)
+df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+
+df_iter = pd.read_csv('green_tripdata_2019-10.csv',iterator=True, chunksize=100000)
+df = next(df_iter)
+
+
+
+print(pd.io.sql.get_schema(df, name = 'green_taxi_data', con=engine))
+
+#df.to_sql(name="green_taxi_data", con=engine, if_exists="replace")
 
